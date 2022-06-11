16bad8dcabebc32dfcc5562a22efb864e0d6cfdd
SujayPrabhu96 <prabhusujay@gmail.com>
2022-06-11 11:33:59 +0530

    created new api and util files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-function App() {
+// components
+import { QuestionCard } from "./components/QuestionCard"
+
+export const App = () => {
+
+  const startTrivia = () => { }
+
+  const nextQuestion = () => { }
+
   return (
     <div className="App">
-      Quiz
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
 
-export default App;
