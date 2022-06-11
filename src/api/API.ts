b89e4a59f1d20ae6cc16bed56d24b044e27413b1
SujayPrabhu96 <prabhusujay@gmail.com>
2022-06-11 19:19:05 +0530

    Make api call and structure response as per required

---
+++ src/api/API.ts
@@ -0,0 +1,29 @@
+import axios from "./axios";
+import { shuffleArray } from "../utils";
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionAnswers = Question & { answers: string[] }
+
+export type Difficulty = "easy" | "medium" | "hard"
+
+export const fetchQuestions = async (amount: number, difficulty: Difficulty) => {
+    try {
+        const { data } = await axios.get(`api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`)
+        return data.results.map((query: Question) => (
+            {
+                ...query,
+                answers: shuffleArray([query.correct_answer, query.incorrect_answers])
+            }
+        ))
+    } catch (error) {
+        throw error;
+    }
+}
